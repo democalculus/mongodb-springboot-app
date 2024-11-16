28b6c778a3c0061ba1bf99dd1a1b4ff3f476bce9
Eghosa <makutaworldmpm@eghosas-mbp.attlocal.net>
2024-11-16 12:41:55 -0600

    push

---
+++ cmSecret-ns.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $? -ne 0 ]]; then
+    echo "please create namespace and configMap for mongodb App"
+    kubectl apply -f cmSecret-ns.yml
+else
+  echo "Namespace already exist please proceed"
+ #kubectl -n ibm-ucd rollout undo deploy ${mss_pod_app}
+ #echo "Deployment ${mss_pod_app} Rollout is Success"
+fi
